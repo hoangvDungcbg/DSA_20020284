0f63ef304c5970100892cecadac977eb2139ad13
hoangvDungcbg <20020284@vnu.edu.vn>
2021-09-24 02:46:03 +0700

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+public class HelloWorld {
+    // Type your main code here
+    public static void main(String[] args) {
+        System.out.println(printHello());
+    }
+
+    public static String printHello() {
+        return "HelloWorld";
+    }
+}
