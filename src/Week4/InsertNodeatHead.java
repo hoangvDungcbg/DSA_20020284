3ab40ea7c00842fe36a02b06e735dad42ba8988a
hoangvDungcbg <20020284@vnu.edu.vn>
2021-10-04 21:51:01 +0700

    Update InsertNodeatHead.java

--- src/Week4/InsertNodeatHead.java
+++ src/Week4/InsertNodeatHead.java
@@ -1,3 +1,14 @@
+public class insertNodeAtHead {
+
+    static class SinglyLinkedListNode {
+        public int data;
+        public SinglyLinkedListNode next;
+
+        public SinglyLinkedListNode(int nodeData) {
+            this.data = nodeData;
+            this.next = null;
+        }
+    }
 static SinglyLinkedListNode insertNodeAtHead(SinglyLinkedListNode llist, int data) {
     SinglyLinkedListNode node=new SinglyLinkedListNode(data);
     node.next=llist;
@@ -5,4 +16,5 @@ static SinglyLinkedListNode insertNodeAtHead(SinglyLinkedListNode llist, int dat
     return llist;
     
 
+}
 }
